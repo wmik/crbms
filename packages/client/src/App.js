a568e4c1d5a69dc0b06dfbbae71d4bf35aca3644
wmik <williemik.wmik@gmail.com>
2019-06-15 10:19:44 +0300

    chore(client): reset styling

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,7 +1,9 @@
+import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import { ApolloProvider, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import ApolloClient from 'apollo-boost';
+import { Grid } from 'semantic-ui-react';
 
 const client = new ApolloClient({
   uri: '/graphql'
@@ -15,21 +17,27 @@ const QUERY_STATUS = gql`
 
 function Status() {
   return (
-    <Query query={QUERY_STATUS}>
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error :(</p>;
+    <Grid.Row>
+      <Grid.Column>
+        <Query query={QUERY_STATUS}>
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
 
-        return <pre>{data.status}</pre>;
-      }}
-    </Query>
+            return <pre>{data.status}</pre>;
+          }}
+        </Query>
+      </Grid.Column>
+    </Grid.Row>
   );
 }
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Status />
+      <Grid stackable>
+        <Status />
+      </Grid>
     </ApolloProvider>
   );
 }
