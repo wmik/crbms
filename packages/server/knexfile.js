4c025dd04f463b8881c3a961e8f8fc2c5394f7b6
wmik <williemik.wmik@gmail.com>
2019-06-15 17:48:18 +0300

    chore(server): configuration
    - Add dev config

--- packages/server/knexfile.js
+++ packages/server/knexfile.js
@@ -14,6 +14,21 @@ module.exports = {
       directory: './database/seeds'
     }
   },
+  dev: {
+    client: 'pg',
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
